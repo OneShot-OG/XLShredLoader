52590ffbb494d3024fa251ad65c958c4ed82ccf5
OneShot-OG <47044106+OneShot-OG@users.noreply.github.com>
2019-01-30 16:54:02 -0500

    added lower high pop

---
+++ XLShredLowerHighPop/Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using Harmony12;
+using UnityModManagerNet;
+using XLShredLib;
+using UnityEngine;
+using XLShredLib.UI;
+
+namespace XLShredLowerHighPop {
+    [Serializable]
+    public class Settings : UnityModManager.ModSettings
+    {
+        public void IncreaseHighPopForce()
+        {
+            this.HighPopForce += 0.05f;
+        }
+
+        public void DecreaseHighPopForce()
+        {
+            this.HighPopForce -= 0.05f;
+        }
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            UnityModManager.ModSettings.Save<Settings>(this, modEntry);
+        }
+
+        public float HighPopForce = 0.3f;
+
+    }
+
+    static class Main
+    {
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Main.settings = UnityModManager.ModSettings.Load<Settings>(modEntry);
+            HarmonyInstance.Create(modEntry.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
+            modEntry.OnSaveGUI = new Action<UnityModManager.ModEntry>(Main.OnSaveGUI);
+            modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(Main.OnToggle);
+            ModMenu.Instance.gameObject.AddComponent<XLShredLowerHighPop>();
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Main.enabled = value;
+            return true;
+        }
+
+        private static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            Main.settings.Save(modEntry);
+        }
+
+        public static bool enabled;
+
+        public static Settings settings;
+    }     
+}
---
+++ XLShredLowerHighPop/XLShredLowerHighPop.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using XLShredLib;
+using XLShredLib.UI;
+
+using System;
+
+namespace XLShredLowerHighPop
+{
+    internal class XLShredLowerHighPop : MonoBehaviour
+    {
+        public void Start()
+        {
+            ModUIBox modUIBox = ModMenu.Instance.RegisterModMaker("OneShot", "OneShot", 0);
+            modUIBox.AddLabel(", / . - Adjust High Pop Force", 0, () => Main.enabled, 0);
+        }
+
+        public void Update()
+        {
+            if (Main.enabled)
+            {
+                ModMenu.Instance.KeyPress(46, 0.2f, delegate ()
+                {
+                    Main.settings.IncreaseHighPopForce();
+                    ModMenu.Instance.ShowMessage("High Pop Force: " + Main.settings.HighPopForce + " Default: 0.75");
+                });
+                ModMenu.Instance.KeyPress(44, 0.2f, delegate ()
+                {
+                    Main.settings.DecreaseHighPopForce();
+                    ModMenu.Instance.ShowMessage("High Pop Force: " + Main.settings.HighPopForce + " Default: 0.75");
+                });
+            }
+        }
+    }
+}
+namespace XLShredLowerHighPop.Patches
+{
+    [HarmonyPatch(typeof(PlayerState_Setup), "OnNextState")]
+    internal static class PlayerState_SetUp_OnNextState_Patch
+    {
+        private static void Prefix(ref float ____highPoPForce)
+        {
+            if (Main.enabled)
+            {
+                ____highPopForce = Main.settings.customHighPopForce;
+            }
+        }
+    }
+}
